756a5eee5c36931592b4a7f3bfc94847e4ec1b91
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2020-12-03 00:52:48 +0530

    style: html and css part completed

---
+++ script.js
@@ -0,0 +1,13 @@
+var input = document.querySelector("#input-text");
+var translateButton = document.querySelector("#translateButton");
+var output = document.querySelector("#output-text");
+
+
+// var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+// function clickHandler() {
+//     console.log("asdfghj");
+// }
+
+
+translateButton.addEventListener("click", clickHandler)
