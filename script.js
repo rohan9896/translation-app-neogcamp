5161c3e6521dde75bc7455a699f016624054d449
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2020-12-03 01:58:54 +0530

    update: script.js

--- script.js
+++ script.js
@@ -3,11 +3,39 @@ var translateButton = document.querySelector("#translateButton");
 var output = document.querySelector("#output-text");
 
 
-// var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var url = "https://api.funtranslations.com/translate/minion.json";
 
-// function clickHandler() {
-//     console.log("asdfghj");
-// }
+
+function urlConstructor(text) {
+    return url + "?text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("error occured "+error);
+    alert("api limit exceeded, try after 60 mins");
+}
+
+
+function clickHandler() {
+
+    var inputText = input.value;
+
+    // console.log(inputText);
+
+    var reqUrl = urlConstructor(inputText);
+
+    fetch(reqUrl)
+    .then(response => response.json())
+    .then(data => {
+        var changedText = data.contents.translated;
+        output.innerText = changedText;
+    })
+    .catch(errorHandler);
+
+
+
+    // output.innerHTML = ;
+}
 
 
 translateButton.addEventListener("click", clickHandler)
